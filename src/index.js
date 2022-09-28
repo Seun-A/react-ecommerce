add56935cdf4e189fd88a508cdc0ffd31da24b60
Seun-A <73300414+Seun-A@users.noreply.github.com>
2022-09-28 19:17:20 +0100

    Redux Persist

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ import SignInUpPage from './pages/sign-in-up/sign-in-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
 
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store'
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -23,35 +24,37 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<App />}>
-            <Route index element={<Homepage />} />
-            <Route path='/shop' element={<ShopPage />} />
-            <Route 
-              exact
-              path='/signin' 
-              element={
-                <SignInUpPage />  // THIS IS WHERE I WANT MY NAVIGATE TO BE 
-              } 
-            />
-            <Route path='/checkout' element={<CheckoutPage />} />
+        <PersistGate persistor={persistor}>
+          <Routes>
+            <Route exact path='/' element={<App />}>
+              <Route index element={<Homepage />} />
+              <Route path='/shop' element={<ShopPage />} />
+              <Route 
+                exact
+                path='/signin' 
+                element={
+                  <SignInUpPage />  // THIS IS WHERE I WANT MY NAVIGATE TO BE 
+                } 
+              />
+              <Route path='/checkout' element={<CheckoutPage />} />
+              {/* No Match */}
+              <Route 
+                path='*'
+                element = {
+                  <div
+                    className='d-flex justify-content-center align-items-center fs-2'
+                  >Can't find the page you're looking for :(</div>
+                }
+              />
+            </Route>
+
             {/* No Match */}
             <Route 
               path='*'
-              element = {
-                <div
-                  className='d-flex justify-content-center align-items-center fs-2'
-                >Can't find the page you're looking for :(</div>
-              }
+              element = {<div>Can't find the page you're looking for :(</div>}
             />
-          </Route>
-
-          {/* No Match */}
-          <Route 
-            path='*'
-            element = {<div>Can't find the page you're looking for :(</div>}
-          />
-        </Routes>
+          </Routes>
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
